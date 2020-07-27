de118cd8301ddc0c0f660935ac4bf1e8425872a6
Qcyan <1165518359@qq.com>
2020-07-27 10:01:00 +0800

    moreLoad

--- src/pages/demo/more-load.vue
+++ src/pages/demo/more-load.vue
@@ -1,77 +1,79 @@
 <template>
   <view>
-    <view v-for="(item, index) in newsList" class="newslist">{{item}}</view>
+    <view v-for="(item, index) in newsList" class="newslist">{{item.author}}</view>
     <view class="loading">{{loadingText}}</view>
   </view>
 </template>
+
 <script>
-  var _self, page = 1, timer = null;
+  var timer = null;
   export default {
     data(){
       return {
         newsList:[],
-        loadingText:'加载中...'
+        loadingText:'加载中...',
+        loadOpts:{
+          page:1,
+        }
       }
-    },
-    onLoad:function(){
-      _self = this;
-      this.getnewsList();
-    },
-    onPullDownRefresh:function(){
-      this.getnewsList();
-    },
-    onReachBottom:function(){
-      if(timer != null){
-        clearTimeout(timer);
-      }
-      timer = setTimeout(function(){
-        _self.getmorenews();
-      }, 1000);
     },
     methods:{
-      getmorenews : function(){
-        if(_self.loadingText != '' && _self.loadingText != '加载更多'){
-          return false;
-        }
-        _self.loadingText = '加载中...';
-        uni.showNavigationBarLoading();
-        uni.request({
-          url: 'https://demo.hcoder.net/index.php?user=hcoder&pwd=hcoder&m=list1&page='+page,
-          method: 'GET',
-          success: function(res){
-            _self.loadingText = '';
-            if(res.data == null){
-              uni.hideNavigationBarLoading();
-              _self.loadingText = '已加载全部';
-              return false;
-            }
-            page++;
-            console.log(res);
-            _self.newsList = _self.newsList.concat(res.data.split('--hcSplitor--'));
-            _self.loadingText = '加载更多';
-            uni.hideNavigationBarLoading();
-          }
-        });
+      onPullDownRefresh:function(){
+        this.getnewsList();
       },
-      getnewsList : function(){
-        page = 1;
+      onReachBottom:function(){
+        if(timer != null){
+          clearTimeout(timer);
+        }
+        timer = setTimeout(function(){
+          this.getmorenews();
+        }.bind(this), 1000);
+      },
+      getmorenews(){
+        if(this.loadingText != '' && this.loadingText != '加载更多'){
+          return false;
+        };
+        this.loadingText = '加载中...';
         uni.showNavigationBarLoading();
-        uni.request({
-          url: 'https://demo.hcoder.net/index.php?user=hcoder&pwd=hcoder&m=list1&page=1',
-          method: 'GET',
-          success: function(res){
-            page++;
-            _self.newsList = res.data.split('--hcSplitor--');
-            uni.hideNavigationBarLoading();
-            uni.stopPullDownRefresh();
-            _self.loadingText = '加载更多';
+        this.$request({
+          url: '/more/list',
+          method: 'get',
+          data:{
+            page: this.loadOpts.page
           }
-        });
+        }).then((res) => {
+          this.loadingText = '';
+          if(res.data == null){
+            uni.hideNavigationBarLoading();
+            this.loadingText = '已加载全部';
+            return false;
+          }
+          this.loadOpts.page++;
+          this.newsList = this.newsList.concat(res.data.list);
+          this.loadingText = '加载更多';
+          uni.hideNavigationBarLoading();
+        })
+      },
+      getnewsList(opts = {}){
+        this.loadOpts.page = 1;
+        uni.showNavigationBarLoading();
+        this.$request({
+          url: '/more/list',
+          method: 'get',
+          data:{
+            page: this.loadOpts.page
+          }
+        }).then((res) => {
+          this.loadOpts.page++;
+          this.newsList = res.data.list;
+          uni.hideNavigationBarLoading();
+          uni.stopPullDownRefresh();
+          this.loadingText = '加载更多';
+        })
       }
+    },
+    onLoad(){
+      this.getnewsList()
     }
   }
 </script>
-<style>
-  .newslist{padding:10px; line-height:60px; font-size:28px;}
-  .loading{text-align:center; line-height:80px;}
-</style>
